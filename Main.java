4350d47b5bbbb087139f9ba9343412bf0a270140
zhanyang <zygoing94@gmail.com>
2014-05-27 23:03:47 +0800

    some bug fix and delete the garbled annotation

--- Main.java
+++ Main.java
@@ -18,11 +18,11 @@ public class Main {
 	static Matrix test_attrs=new Matrix(test_instance_num,attr_num+1,1);	
 	static Matrix test_types=new Matrix(test_instance_num,1);
 	
-	static Matrix[] weighs=new Matrix[type_num];	//写入文件
+	static Matrix[] weighs=new Matrix[type_num];	
 	
-	static double[] history=new double[100];	//写入文件
+	static double[] history=new double[100];	
 	static double[] prediction_score=new double[test_instance_num];
-	static int[] prediction_type=new int[test_instance_num];	//写入文件
+	static int[] prediction_type=new int[test_instance_num];	
 	
 	
 	public static void main(String[] args) throws IOException {
@@ -35,11 +35,7 @@ public class Main {
 			training(i, 100);
 			System.out.println("Complete "+(i+1)+" times ");
 		}
-		FileWriter fw=new FileWriter("D:\\Zhanyang\\Classification\\results\\history.txt");
-		for(int i=0;i<100;i++){
-			fw.write(""+history[i]+"\n");
-		}
-		fw.close();
+		
 		testing("D:\\Zhanyang\\Classification\\test.data");
 		Calendar cal=Calendar.getInstance();
 		System.out.println("Time cost:　"+(cal.getTimeInMillis()-c.getTimeInMillis())/1000+"  s");
@@ -71,24 +67,31 @@ public class Main {
 		}
 		
 		for(int i=0;i<times;i++){
-			history[i]=weighs[0].get(0, 0);
+			history[i]=weighs[type].get(0, 0);
 			error=current_type.minus(return_type(training_attrs.times(weighs[type])));
 			weighs[type].plusEquals(training_attrs.transpose().times(a).times(error));
 		}
 		
-		FileWriter fw=new FileWriter("D:\\Zhanyang\\Classification\\results\\weighs.txt");
+		FileWriter fw=new FileWriter("D:\\Zhanyang\\Classification\\results\\history"+(type+1)+".txt");
+		for(int i=0;i<100;i++){
+			fw.write(""+history[i]+"\n");
+		}
+		fw.close();
+		
+		FileWriter fw2=new FileWriter("D:\\Zhanyang\\Classification\\results\\weighs"+(type+1)+".txt");
 		for(int i=0;i<weighs.length;i++){
 			fw.write(""+weighs[i]+"\n");
 		}
-		fw.close();
+		fw2.close();
 	}
 	
 	public static Matrix return_type(Matrix todo){		
 		for(int i=0;i<training_instance_num;i++){
-			if(todo.get(i, 0)>0)
-				todo.set(i, 0, 1);
-			else
-				todo.set(i, 0, 0);
+//			if(todo.get(i, 0)>0)
+//				todo.set(i, 0, 1);
+//			else
+//				todo.set(i, 0, 0);
+			todo.set(i, 0, 1/(1+Math.pow(Math.E,-todo.get(i, 0))));
 		}
 		return todo;
 	}
@@ -113,7 +116,7 @@ public class Main {
 		int max_type=0;
 		for(int i=0;i<test_instance_num;i++){
 			for(int j=0;j<7;j++){
-				Matrix score=(test_attrs.getMatrix(new int[]{i}, 0, attr_num-1)).times(weighs[j]);
+				Matrix score=(test_attrs.getMatrix(new int[]{i}, 0, attr_num)).times(weighs[j]);
 				if(score.get(0, 0)>max_score){
 					max_score=score.get(0, 0);
 					max_type=(j+1);
@@ -133,9 +136,9 @@ public class Main {
 	}
 	
 	public static void calculate_F1() throws IOException{
-		double[] Macro_f1s=new double[7];	//写入文件
-		double[] precisions=new double[7];	//写入文件
-		double[] recalls=new double[7];	//写入文件
+		double[] Macro_f1s=new double[7];	
+		double[] precisions=new double[7];	
+		double[] recalls=new double[7];	
 		
 		int[] every_test_type=new int[7];
 		int[] every_predict_type=new int[7];
@@ -144,10 +147,10 @@ public class Main {
 		for(int i=0;i<test_instance_num;i++){
 			int t=(int) test_types.get(i, 0);
 			int p=prediction_type[i];
-			every_test_type[t]++;
-			every_predict_type[p]++;
+			every_test_type[t-1]++;
+			every_predict_type[p-1]++;
 			if(t==p)
-				all_right_type[t]++;
+				all_right_type[t-1]++;
 		}
 		
 		for(int i=0;i<7;i++){
